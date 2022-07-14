d0f94662519f0ac519e5f15df61bf0e6cf4cd563
OlgaVeronica <olgavas.opo@gmail.com>
2022-07-14 17:14:50 -0400

    0101-0201

---
+++ JavaScript Completo ES6/Modulo 2/0201 Variáveis/script.js
@@ -0,0 +1,18 @@
+var nome='Olga' ;
+var idade = '18';
+var possuiFaculdade = false;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+console.log(totalPreco);
+
+var sobrenome = 'Veronica', cidade = 'Ouro Preto do Oeste';
+
+console.log(sobrenome, cidade)
+
+var semDefinir;
+
+console.log(semDefinir)
