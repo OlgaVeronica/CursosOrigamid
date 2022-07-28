17c4d1488edc67851f46cf893376a4608e989c94
OlgaVeronica <olgavas.opo@gmail.com>
2022-07-27 22:19:59 -0400

    0209

---
+++ JavaScript Completo ES6/Modulo 2/0209 Atribuição e Ternário/script.js
@@ -0,0 +1,21 @@
+var numero = 20;
+var numero2 = 10;
+
+numero += numero2//numero = numero + 10
+
+console.log(numero)
+console.log(numero2)
+
+var naoPossuiDiabetes = false
+var idade =20
+
+var podeBeber;
+podeBeber = (idade >= 18 && naoPossuiDiabetes) ? 'pode beber' : 'nao pode beber'
+console.log(podeBeber)
+
+var possuiFaculdade = true;
+
+if(possuiFaculdade)
+    console.log('sim possui')
+else
+    console.log('nao possui')
