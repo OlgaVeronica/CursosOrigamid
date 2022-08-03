0cc627b9154a7075f4338ed8c54cee46b81a5d04
Olga Veronica <olgavas.opo@gmail.com>
2022-08-03 15:19:31 -0400

    0303-0304

--- JavaScript Completo ES6/Modulo 3/0303 Seleção de Elementos 1/script/script.js
+++ JavaScript Completo ES6/Modulo 3/0303 Seleção de Elementos 1/script/script.js
@@ -15,3 +15,21 @@ console.log(linksInternos.href)
 
 const animaisImg = document.querySelectorAll('.animais img')
 console.log(animaisImg[1])
+
+const gridSectionHTML = document.getElementsByClassName('grid-section')
+const gridSectionNode = document.querySelectorAll('.grid-section')
+
+primeiraUl.classList.add('grid-section')
+
+// console.log(gridSectionHTML[0])
+// console.log(gridSectionNode[0])
+
+gridSectionNode.forEach(function(item, index){
+    console.log(item);
+})
+
+const arrayGrid = Array.from(gridSectionHTML)
+
+arrayGrid.forEach(function(item){
+    console.log(item)
+})
---
+++ JavaScript Completo ES6/Modulo 3/0304 forEach e Arrow Function/script/script.js
@@ -0,0 +1,34 @@
+// const imgs = document.querySelectorAll('img')
+// // console.log(imgs)
+
+// let i = 0
+// imgs.forEach(function(item, index, array){
+//     // console.log(item, index, array)
+// })
+
+const titulos = document.getElementsByClassName('titulo');
+const titulosArray = Array.from(titulos);
+
+// console.log(titulos)
+// console.log(titulosArray)
+
+titulosArray.forEach(function(item, index){
+//   console.log(item, index);
+});
+
+const imgs = document.querySelectorAll('img')
+// console.log(imgs)
+
+imgs.forEach((item, index) => {
+    console.log(item, index)
+})
+
+let i = 0;
+imgs.forEach(item => {
+    return console.log(i++)
+})// um argumento apenas nao precisa de parenteses, nenhum ou mais de um precisam sempre
+
+imgs.forEach(item => console.log(i++))
+
+
+
