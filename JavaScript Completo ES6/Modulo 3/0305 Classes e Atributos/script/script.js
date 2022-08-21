dd4bc7568acae06706f8e32e02f942894180509e
OlgaVeronica <olgavas.opo@gmail.com>
2022-08-20 22:02:28 -0400

    0305-0306

--- JavaScript Completo ES6/Modulo 3/0305 Classes e Atributos/script/script.js
+++ JavaScript Completo ES6/Modulo 3/0305 Classes e Atributos/script/script.js
@@ -1,6 +1,6 @@
 const menu = document.querySelector('.menu')
 
- menu.classList.add('ativo', 'ativo')
+ menu.classList.add('ativo', 'teste')
  menu.classList.remove('azul')
  menu.classList.toggle('azul')
 
@@ -8,5 +8,26 @@ const menu = document.querySelector('.menu')
     menu.classList.add('possui-azul')
  else
  menu.classList.add('nao-possui-azul')
-console.log(menu.className)
-menu.className
+//  menu.className = menu.className + ' vermelho'
+//  menu.className += ' vermelho'
+//  console.log(menu.className)
+
+const animais =  document.querySelector('.animais')
+console.log(animais.attributes['data-texto'])
+
+const img = document.querySelector('img')
+
+const srcImg = img.getAttribute('alt')
+
+img.setAttribute('alt', 'é uma raposa')
+const possuiAlt = img.hasAttribute('alt')
+console.log(possuiAlt)
+
+console.log(img.getAttribute('alt'))
+
+const carro = {
+   portas: 4,
+   andar(km){
+      console.log(`andou ${km}`)
+   }
+}
